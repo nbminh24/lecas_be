e3a34f7110cb315a3935e39b9c031ed3534348e5
nbminh24 <nbminh24@gmail.com>
2025-07-08 14:42:14 +0700

    Fix Bad Auth Error

--- be_lecas/Repositories/UserRepository.cs
+++ be_lecas/Repositories/UserRepository.cs
@@ -11,7 +11,8 @@ namespace be_lecas.Repositories
         public UserRepository(IConfiguration configuration)
         {
             var client = new MongoClient(configuration.GetConnectionString("MongoDB"));
-            var database = client.GetDatabase("lecas");
+            var databaseName = configuration.GetSection("ConnectionStrings:DatabaseName").Value ?? "lecas";
+            var database = client.GetDatabase(databaseName);
             _users = database.GetCollection<User>("users");
         }
 
